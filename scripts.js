63da66d396a857e98bda69d9152d59281fe992d4
Tetiana Pomohaieva <t.pomogaeva@gmail.com>
2020-10-24 20:59:34 +0300

    optimized js

--- scripts.js
+++ scripts.js
@@ -1,207 +1,197 @@
 // Task 1 - user age
-let userAge = prompt('How old are you?');
+const userAge = prompt('How old are you?');
+let message1 = document.querySelector('#user-age');
 
-if (userAge <= 12 && userAge >= 0) {
-    document.getElementById("user-age").innerHTML =
-    'You are a child!';
+if (isNaN(userAge) || userAge > 130 || userAge < 0) {
+	message1.innerHTML = 'Please enter a valid age.';
+} else if (userAge <= 12 && userAge >= 0) {
+	message1.innerHTML = 'You are a child!';
 } else if (userAge > 12 && userAge <= 18) {
-    document.getElementById("user-age").innerHTML =
-    'You are a teenager!';
+	message1.innerHTML = 'You are a teenager!';
 } else if (userAge > 18 && userAge <= 60) {
-    document.getElementById("user-age").innerHTML =
-    'You are an adult!';
-} else if (userAge > 60 && userAge < 130) {
-    document.getElementById("user-age").innerHTML =
-    'You are a retiree!';
+	message1.innerHTML = 'You are an adult!';
 } else {
-    document.getElementById("user-age").innerHTML =
-    'Please enter a valid age.';
+	message1.innerHTML = 'You are a retiree!';
 }
 
 
 // Task 2 - keyboard symbols
 let keyboardSymbol = prompt('Enter any number from 0 to 9');
+let message2 = document.querySelector('#symbol');
 
 switch (keyboardSymbol) {
-  case '1':
-    document.getElementById("symbol").innerHTML = 'Your symbol is !';
-    break;
-  case '2':
-    document.getElementById("symbol").innerHTML = 'Your symbol is "';
-    break;
-  case '3':
-    document.getElementById("symbol").innerHTML = 'Your symbol is £';
-    break;
-  case '4':
-    document.getElementById("symbol").innerHTML = 'Your symbol is $';
-    break;
-  case '5':
-    document.getElementById("symbol").innerHTML = 'Your symbol is %';
-    break;
-  case '6':
-    document.getElementById("symbol").innerHTML = 'Your symbol is ^';
-    break;
-  case '7':
-    document.getElementById("symbol").innerHTML = 'Your symbol is &';
-    break;
-  case '8':
-    document.getElementById("symbol").innerHTML = 'Your symbol is *';
-    break;
-  case '9':
-    document.getElementById("symbol").innerHTML = 'Your symbol is (';
-    break;
-  case '0':
-    document.getElementById("symbol").innerHTML = 'Your symbol is )';
-    break;
-  default:
-    document.getElementById("symbol").innerHTML = 'Invalid input.';
+	case '1':
+		message2.innerHTML = 'Your symbol is !';
+		break;
+	case '2':
+		message2.innerHTML = 'Your symbol is "';
+		break;
+	case '3':
+		message2.innerHTML = 'Your symbol is £';
+		break;
+	case '4':
+		message2.innerHTML = 'Your symbol is $';
+		break;
+	case '5':
+		message2.innerHTML = 'Your symbol is %';
+		break;
+	case '6':
+		message2.innerHTML = 'Your symbol is ^';
+		break;
+	case '7':
+		message2.innerHTML = 'Your symbol is &';
+		break;
+	case '8':
+		message2.innerHTML = 'Your symbol is *';
+		break;
+	case '9':
+		message2.innerHTML = 'Your symbol is (';
+		break;
+	case '0':
+		message2.innerHTML = 'Your symbol is )';
+		break;
+	default:
+		message2.innerHTML = 'Invalid input.';
 }
 
 
-// Task 3 - check a number for repeated digits
+// Task 3 - check the number for repeated digits
 let userNumber = prompt('Please, enter 3-digit number');
+let message3 = document.querySelector('#number');
 
 if (isNaN(userNumber) || userNumber.length != 3) {
-    document.getElementById("number").innerHTML = 
-    'Invalid input. Please make sure you use digits only, and your number contains 3 digits!';
-} else if (userNumber.length = 3 && (userNumber[0] == userNumber[1] || userNumber[1] == userNumber[2] || userNumber[0] == userNumber[2] )) {
-  document.getElementById("number").innerHTML = 
-  'This number has repeated digits.';
+	message3.innerHTML = 'Invalid input. Please make sure you use digits only, and your number contains 3 digits!';
+} else if (userNumber.length = 3 && (userNumber[0] == userNumber[1] || userNumber[1] == userNumber[2] || userNumber[0] == userNumber[2])) {
+	message3.innerHTML = 'This number has repeated digits.';
 } else {
-  document.getElementById("number").innerHTML = 
-  'This number does not have repeated digits.';
+	message3.innerHTML = 'This number does not have repeated digits.';
 }
 
 
-// Task 4 - check if a year is a leap year or not
+// Task 4 - check if the given year is a leap year or not
 let year = prompt('Enter a year');
+let message4 = document.querySelector('#year')
 
 if (year % 4 != 0) {
-  document.getElementById("year").innerHTML = 'It is a common year!';
+	message4.innerHTML = 'It is a common year!';
 } else if (year % 25 != 0) {
-  document.getElementById("year").innerHTML = 'It is a leap year!';
+	message4.innerHTML = 'It is a leap year!';
 } else if (year % 16 != 0) {
-  document.getElementById("year").innerHTML = 'It is a common year!';
+	document.getElementById("year").innerHTML = 'It is a common year!';
 } else {
-  document.getElementById("year").innerHTML = 'It is a leap year!';
+	document.getElementById("year").innerHTML = 'It is a leap year!';
 }
 
 
 // Task 5 - palindrome or not
-let value = prompt ('Enter a 5-digit number');
+let value = prompt('Enter a 5-digit number');
+let message5 = document.querySelector('#palindrome');
 
 if (isNaN(value) || value.length != 5) {
-    document.getElementById("palindrome").innerHTML = 
-    'Invalid input. Please make sure you use digits only, and your number contains 5 digits!';
+	message5.innerHTML = 'Invalid input. Please make sure you use digits only, and your number contains 5 digits!';
 } else if (value[0] == value[4] && value[1] == value[3]) {
-    document.getElementById("palindrome").innerHTML = 
-    'It is a palindrome!';
+	message5.innerHTML = 'It is a palindrome!';
 } else {
-    document.getElementById("palindrome").innerHTML = 
-    'It is not a palindrome!';
+	message5.innerHTML = 'It is not a palindrome!';
 }
 
 
 // Task 6 - currency exchange tool
-let exchangeValueFrom = prompt ('Enter amount in USD');
-let currency = prompt ('Choose the currency to convert: EUR, UAH, AZN');
-let exchangeValueTo = currency.toUpperCase();
+let exchangeValueFrom = prompt('Enter amount in USD');
+let currency = prompt('Choose the currency to convert: EUR, UAH, AZN');
+let exchangeValueTo = currency.tolowerCase();
+let message6 = document.querySelector('#exchange');
 
 if (isNaN(exchangeValueFrom)) {
-    document.getElementById('exchange').innerHTML = 
-    'Invalid input. Amount should be a number!';
-} else if (exchangeValueTo == 'EUR') { 
-    document.getElementById('exchange').innerHTML = 
-    `For ${exchangeValueFrom} USD you will get ${Math.round(exchangeValueFrom * 0.85)} ${exchangeValueTo}.`;
+	message6.innerHTML = 'Invalid input. Amount should be a number!';
+} else if (exchangeValueTo == 'eur') {
+	message6.innerHTML = `For ${exchangeValueFrom} USD you will get ${Math.round(exchangeValueFrom * 0.85)} ${exchangeValueTo.toUpperCase()}.`;
 } else if (exchangeValueTo == 'UAH') {
-    document.getElementById('exchange').innerHTML = 
-    `For ${exchangeValueFrom} USD you will get ${Math.round(exchangeValueFrom * 28.28)} ${exchangeValueTo}.`;
+	message6.innerHTML = `For ${exchangeValueFrom} USD you will get ${Math.round(exchangeValueFrom * 28.28)} ${exchangeValueTo.toUpperCase()}.`;
 } else if (exchangeValueTo == 'AZN') {
-    document.getElementById('exchange').innerHTML = 
-    `For ${exchangeValueFrom} USD you will get ${Math.round(exchangeValueFrom * 1.7)} ${exchangeValueTo}.`;
+	message6.innerHTML = `For ${exchangeValueFrom} USD you will get ${Math.round(exchangeValueFrom * 1.7)} ${exchangeValueTo.toUpperCase()}.`;
 } else {
-    document.getElementById('exchange').innerHTML = 
-    'Sorry, we do not support this currency exchange calculation.';
+	message6.innerHTML = 'Sorry, we do not support this currency exchange calculation.';
 }
 
 
 // Task 7 - discount
-let price = prompt ('Enter the original price');
+let price = prompt('Enter the original price');
+let message7 = document.querySelector('#discount-price');
 
 if ((price > 0) && (price < 200)) {
-    document.getElementById("discount-price").innerHTML = 
-    `The price is ${price}.`;
+	message7.innerHTML = `The price is ${price}.`;
 } else if ((price >= 200) && (price <= 300)) {
-    let discount = price * .03;
-    let priceWithDiscount = price - discount;
-    document.getElementById("discount-price").innerHTML = 
-    `The price is ${priceWithDiscount}.`;
+	let discount = price * .03;
+	let priceWithDiscount = price - discount;
+	message7.innerHTML = `The price is ${priceWithDiscount}.`;
 } else if ((price > 300) && (price <= 500)) {
-    let discount = price * .05;
-    let priceWithDiscount = price - discount;
-    document.getElementById("discount-price").innerHTML = 
-    `The price is ${priceWithDiscount}.`;
+	let discount = price * .05;
+	let priceWithDiscount = price - discount;
+	message7.innerHTML = `The price is ${priceWithDiscount}.`;
 } else if ((price > 500)) {
-    let discount = price * .03;
-    let priceWithDiscount = price - discount;
-    document.getElementById("discount-price").innerHTML = 
-    `The price is ${priceWithDiscount}.`;
+	let discount = price * .03;
+	let priceWithDiscount = price - discount;
+	message7.innerHTML = `The price is ${priceWithDiscount}.`;
 } else {
-    document.getElementById("discount-price").innerHTML = 
-    'Please check and enter the original price again.';
+	message7.innerHTML = 'Please check and enter the original price again.';
 }
 
 
 // Task 8 - circles inscribed inside squares
-let circleLength = prompt ('Enter the circle length');
-let squarePerimeter = prompt ('Enter the square perimeter');
+let circleLength = prompt('Enter the circle length');
+let squarePerimeter = prompt('Enter the square perimeter');
 const pi = 3.14;
 
 let circleDiameter = circleLength / pi;
 let squareSide = squarePerimeter / 4;
 
+let message8 = document.querySelector('#inscribed-circle');
+
 if (isNaN(circleLength) || isNaN(squarePerimeter)) {
-   document.getElementById("inscribed-circle").innerHTML = 
-   'Invalid input. Please try again!';
+	message8.innerHTML = 'Invalid input. Please try again!';
 } else if (circleDiameter == squareSide) {
-    document.getElementById("inscribed-circle").innerHTML = 
-    'You can inscribe this circle inside the square.';
+	message8.innerHTML = 'You can inscribe this circle inside the square.';
 } else {
-    document.getElementById("inscribed-circle").innerHTML = 
-    'You cannot inscribe this circle inside the square.';
+	message8.innerHTML = 'You cannot inscribe this circle inside the square.';
 }
 
 
 // Task 9 - quiz
-let questionOne = prompt ('Where is Guatemala? Possible options: North America, Europe, South America');
-let questionTwo = prompt ('Where is Eritrea? Possible options: South America, Africa, Europe');
-let questionThree = prompt ('Where is Lebanon? Possible options: Africa, Asia, Europe');
+let questionOne = prompt('Where is Guatemala? Possible options: North America, Europe, South America');
+let questionTwo = prompt('Where is Eritrea? Possible options: South America, Africa, Europe');
+let questionThree = prompt('Where is Lebanon? Possible options: Africa, Asia, Europe');
 
 let answerOne = questionOne.toUpperCase();
 let answerTwo = questionTwo.toUpperCase();
 let answerThree = questionThree.toUpperCase();
 
 let score = 0;
+const points = 2;
 
-if ((answerOne) == 'SOUTH AMERICA') { 
-    score++; 
+let message9 = document.querySelector('#quiz-score');
+
+if ((answerOne) == 'SOUTH AMERICA') {
+	score++;
 }
 
 if ((answerTwo) == 'AFRICA') {
-    score++;
+	score++;
 }
 
 if ((answerThree) == 'ASIA') {
-    score++;
+	score++;
 }
 
-document.getElementById("quiz-score").innerHTML = `Your score is ${score * 2}.`;
+message9.innerHTML = `Your score is ${score * points}.`;
 
 
-// Task 10 - tomorrow date
-let userValue = prompt ('Enter a date');
+// Task 10 - next date
+let userValue = prompt('Enter a date');
 let userDate = new Date(userValue);
-let nextDate = new Date (userDate);
-nextDate.setDate(userDate.getDate() +1);
+let nextDate = new Date(userDate);
+nextDate.setDate(userDate.getDate() + 1);
 
-document.getElementById("next-date").innerHTML = `You entered ${userDate.toDateString()}. The next date is ${nextDate.toDateString()}.`;
+let message10 = document.querySelector('#next-date');
+
+message10.innerHTML = `You entered ${userDate.toDateString()}. The next date is ${nextDate.toDateString()}.`;
